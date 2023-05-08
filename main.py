8fa47f201786fd4c9fa56a8070eadf6b7d303ce6
redrum88 <117243822+redrum88@users.noreply.github.com>
2023-05-08 14:05:22 +0100

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, flash, Blueprint
+from website import create_app
+from flask import send_from_directory
+import os
+import glob
+import sys
+import binascii
+import argparse
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
